cb862cc88918352bf77aa2e17987f98d16d82ae2
Cruz Gonzalez <cruzgonzalez888@gmail.com>
2021-10-17 17:45:14 -0400

    First commit

---
+++ HW 3/HW_3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3App.swift
+//  HW 3
+//
+//  Created by Cruz Gonzalez Garcia on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct HW_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
